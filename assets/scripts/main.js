0b9d6b2e5146e8e7f33efd344d437d3849255f7e
samaahmed1 <sama34777@gmail.com>
2025-01-22 10:32:17 +0200

    Add: User-Data Section

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -7,3 +7,33 @@ document.getElementById("toggle-theme").onclick = () => {
     document.getElementById("toggle-theme").src = "assets/icons/sun.webp";
   }
 };
+function getData() {
+  fetch(`https://api.github.com/users/samaahmed1`)
+    .then((response) => response.json())
+    .then((data) => {
+      document.getElementById("user-data").innerHTML = `
+        <div class= "avatar">
+        <img src= "${data.avatar_url}" alt="data.name" />
+        <h1>${data.name}</h1>
+        <a href= "${data.html_url}" target="_blank">GitHub</a>
+        </div>
+        <ol id= "user-repos" class= "user-repos container"></ol>
+        `;
+    });
+  fetch(`https://api.github.com/users/samaahmed1/repos`)
+    .then((response) => response.json())
+    .then((data) => {
+      let repos = "";
+      for (let i = 0; i < data.length; i++) {
+        repos += `
+        <li>
+        <a href="${data[i].homepage}" target="_blank">
+          ${data[i].name}
+        </a>
+      </li>
+        `;
+      }
+      document.getElementById("user-repos").innerHTML = repos;
+    });
+}
+getData();
